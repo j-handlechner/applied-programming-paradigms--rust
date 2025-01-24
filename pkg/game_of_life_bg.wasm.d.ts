52b0de176917ec4c336a14cfe068506f493b4da1
Johannes Handlechner <90871582+j-handlechner@users.noreply.github.com>
2025-01-24 15:00:48 +0100

    Add files via upload

---
+++ pkg/game_of_life_bg.wasm.d.ts
@@ -0,0 +1,13 @@
+/* tslint:disable */
+/* eslint-disable */
+export const memory: WebAssembly.Memory;
+export const __wbg_gameoflifecell_free: (a: number, b: number) => void;
+export const __wbg_gameoflife_free: (a: number, b: number) => void;
+export const gameoflife_new: (a: number, b: number, c: number) => number;
+export const gameoflife_reset_cells: (a: number) => void;
+export const gameoflife_life: (a: number, b: number, c: any) => void;
+export const gameoflife_append_life_iteration: (a: number, b: any) => void;
+export const __wbindgen_exn_store: (a: number) => void;
+export const __externref_table_alloc: () => number;
+export const __wbindgen_export_2: WebAssembly.Table;
+export const __wbindgen_start: () => void;
